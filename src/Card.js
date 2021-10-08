5a0d2b4bdba6ba2b4a2a9d50aa61e868afdeda0f
VenkateshGopa <85928554+VenkateshGopa@users.noreply.github.com>
2021-10-08 12:08:11 +0530

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -11,18 +11,11 @@ const Card = (props) => {
   return (
     <div className="pri col-lg-4">
       <div className="card mb-5 mb-lg-0 bg-white">
-      <h5 className="card-title text-muted text-uppercase text-center">{props.name}</h5>
-      <h6 className="card-price text-center">{props.price}<span className="h6">/month</span></h6>
+      <h5 className="card-title text-muted text-uppercase text-center">{props.data.name}</h5>
+      <h6 className="card-price text-center">{props.data.price}<span className="h6">/month</span></h6>
       <hr />
       <ul className="fa-ul">
-        <li className={classcheck(props.usersc)}><span className="fa-li">{checkmark(props.usersc)}</span>{props.users}</li>
-        <li className={classcheck(props.storagec)}><span className="fa-li">{checkmark(props.storagec)}</span>{props.storage}</li>
-        <li className={classcheck(props.projectc)}><span className="fa-li">{checkmark(props.projectc)}</span>{props.project}</li>
-        <li className={classcheck(props.accessc)}><span className="fa-li">{checkmark(props.accessc)}</span>{props.access}</li>
-        <li className={classcheck(props.privateprojectsc)}><span className="fa-li">{checkmark(props.privateprojectsc)}</span>{props.privateprojects}</li>
-        <li className={classcheck(props.supportc)}><span className="fa-li">{checkmark(props.supportc)}</span>{props.support}</li>
-        <li className={classcheck(props.subdomainc)}><span className="fa-li">{checkmark(props.subdomainc)}</span>{props.subdomain}</li>
-        <li className={classcheck(props.reportsc)}><span className="fa-li">{checkmark(props.reportsc)}</span>{props.reports}</li>
+        {props.data.features.map( ele => <li key={ele.name} className={classcheck(ele.valid)}><span className="fa-li">{checkmark(ele.valid)}</span>{ele.name}</li> )}
       </ul>
       <div className="d-grid">
         <a href="www.google.com" className="btn btn-primary text-uppercase">Button</a>
