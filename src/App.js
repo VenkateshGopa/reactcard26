315e348a371febdb3ada0d58af42859297b8d563
VenkateshGopa <85928554+VenkateshGopa@users.noreply.github.com>
2021-10-08 12:08:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,132 @@
-import React from "react";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Card.css";
-const Card = (props) => {
-  const checkmark = (props) =>{
-   return  props === "true" ? <i className="fas fa-check"></i> :<i className="fas fa-times"></i> ;
-  }
-  const classcheck = (props) =>{
-    return props === "true" ? "" :"text-muted";
-  }
+import Card from "./Card";
+const data = [
+  {
+    name: "FREE",
+    price: "$0",
+    features: [
+      {
+        name: "Single User",
+        valid: "true",
+      },
+      {
+        name: "5GB Storage",
+        valid: "true",
+      },
+      {
+        name: "Unlimited Public Projects",
+        valid: "true",
+      },
+      {
+        name: "Community Access",
+        valid: "true",
+      },
+      {
+        name: "Unlimited Private Projects",
+        valid: "false",
+      },
+      {
+        name: "Dedicated Phone Support",
+        valid: "false",
+      },
+      {
+        name: "Free Subdomain",
+        valid: "false",
+      },
+      {
+        name: "Monthly Status Reports",
+        valid: "false",
+      },
+    ],
+  },
+  {
+    name: "PLUS",
+    price: "$9",
+    features: [
+      {
+        name: "5 Users",
+        valid: "true",
+      },
+      {
+        name: "50GB Storage",
+        valid: "true",
+      },
+      {
+        name: "Unlimited Public Projects",
+        valid: "true",
+      },
+      {
+        name: "Community Access",
+        valid: "true",
+      },
+      {
+        name: "Unlimited Private Projects",
+        valid: "true",
+      },
+      {
+        name: "Dedicated Phone Support",
+        valid: "true",
+      },
+      {
+        name: "Free Subdomain",
+        valid: "true",
+      },
+      {
+        name: "Monthly Status Reports",
+        valid: "false",
+      },
+    ],
+  },
+  {
+    name: "Pro",
+    price: "$49",
+    features: [
+      {
+        name: "unlimited User",
+        valid: "true",
+      },
+      {
+        name: "150GB Storage",
+        valid: "true",
+      },
+      {
+        name: "Unlimited Public Projects",
+        valid: "true",
+      },
+      {
+        name: "Community Access",
+        valid: "true",
+      },
+      {
+        name: "Unlimited Private Projects",
+        valid: "true",
+      },
+      {
+        name: "Dedicated Phone Support",
+        valid: "true",
+      },
+      {
+        name: " unlimited Free Subdomain",
+        valid: "true",
+      },
+      {
+        name: "Monthly Status Reports",
+        valid: "true",
+      },
+    ],
+  },
+];
+function App() {
   return (
-    <div className="pri col-lg-4">
-      <div className="card mb-5 mb-lg-0 bg-white">
-      <h5 className="card-title text-muted text-uppercase text-center">{props.data.name}</h5>
-      <h6 className="card-price text-center">{props.data.price}<span className="h6">/month</span></h6>
-      <hr />
-      <ul className="fa-ul">
-        {props.data.features.map( ele => <li key={ele.name} className={classcheck(ele.valid)}><span className="fa-li">{checkmark(ele.valid)}</span>{ele.name}</li> )}
-      </ul>
-      <div className="d-grid">
-        <a href="www.google.com" className="btn btn-primary text-uppercase">Button</a>
-      </div>
+    <div className="top py-5">
+      <div className=" container">
+        <div className="row justify-content-center">
+          { data.map( ele => <Card key= {ele.name} data={ele}/>)}
+        </div>
       </div>
     </div>
   );
-};
-export default Card;
+}
+
+export default App;
